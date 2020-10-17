126dd9a588e2ce42b4e13b13b5fc58a19d1f588f
ycvs212 <59155778+ycvs212@users.noreply.github.com>
2020-10-17 16:03:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
-grove.onGesture(GroveGesture.Wave, function () {
-    basic.showString("HEllo")
+grove.onGesture(GroveGesture.Down, function () {
+    basic.showString("down")
 })
 grove.onGesture(GroveGesture.Right, function () {
     basic.showString("Right")
 })
+grove.onGesture(GroveGesture.Backward, function () {
+    basic.showString("get out")
+})
 grove.onGesture(GroveGesture.Clockwise, function () {
     basic.showIcon(IconNames.Heart)
+})
+grove.onGesture(GroveGesture.Up, function () {
+    basic.showString("up")
+})
+grove.onGesture(GroveGesture.Forward, function () {
+    basic.showString("come on")
 })
 grove.onGesture(GroveGesture.Left, function () {
     basic.showString("Left")

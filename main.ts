488ab39e2cc727901eb860ef4d09388e3d8b39db
ycvs212 <59155778+ycvs212@users.noreply.github.com>
2020-10-18 09:11:11 +0800

    direction

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 grove.onGesture(GroveGesture.Down, function () {
-    basic.showString("down")
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+})
+grove.onGesture(GroveGesture.Wave, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
 grove.onGesture(GroveGesture.Right, function () {
-    basic.showString("Right")
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
 })
 grove.onGesture(GroveGesture.Backward, function () {
     basic.showString("get out")
@@ -11,14 +32,27 @@ grove.onGesture(GroveGesture.Clockwise, function () {
     basic.showIcon(IconNames.Heart)
 })
 grove.onGesture(GroveGesture.Up, function () {
-    basic.showString("up")
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 })
 grove.onGesture(GroveGesture.Forward, function () {
     basic.showString("come on")
 })
 grove.onGesture(GroveGesture.Left, function () {
-    basic.showString("Left")
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
 })
 grove.onGesture(GroveGesture.Anticlockwise, function () {
     basic.showIcon(IconNames.No)
 })
+grove.initGesture()
